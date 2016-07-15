3173c5279f9f7afcc33473b44ae944e99a1e2dae
mattpennella <mattvpennella@gmail.com>
2016-07-15 15:41:16 -0400

    Added angular to the page and pulled in student data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-(funtion(angular){
+/**
+ * Created by mattpennella on 7/15/16.
+ */
+(function(angular) {
+    'use strict'
+    angular.module('bankingSimulatorApp', ['ngRoute', 'controllers', 'services']);
+    angular.module('bankingSimulatorApp').config(function($routeProvider, $sceProvider) {
+        $sceProvider.enabled(false);
+
+        $routeProvider.when('/Student/:studentId/transaction', {
+            templateUrl: 'partials/transaction.html'
+        }).otherwise ({
+            templateUrl: 'partials/transaction.html'
+        });
+    });
 
 }(window.angular));
--- js/controllers/bankingSimulatorController.js
+++ js/controllers/bankingSimulatorController.js
@@ -5,11 +5,14 @@
     'use strict'
     angular.module('bankingSimulatorController', ['studentService'])
     .controller('bankingSimulatorController', ['$scope', '$http', 'studentService', '$q', function($scope, $http, studentService, $q) {
+
         $scope.loadStudentData = function() {
             var studentDataPromise = [studentService.getStudent($scope.student)];
             $q.all(studentDataPromise).then(function(studentDataArray) {
-                $scope.studentDate = studentDataArray[0].data;
+                $scope.studentData = studentDataArray[0].data;
             })
         };
+
+        $scope.loadStudentData();
     }]);
 }(window.angular));
