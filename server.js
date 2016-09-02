08ac03c0d5eca110e5446119d4caae6bd48ea41e
Rob Zahorchak <tuf33757@temple.edu>
2016-09-02 13:48:53 -0400

    Adjusted the landing page css to start using full bootstrap responsive design

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/public/index.html'));
 
+
 var port = process.env.PORT || 3030;
 app.listen(port, function() {
     console.log('Listening on port ' + port);
 });
-
