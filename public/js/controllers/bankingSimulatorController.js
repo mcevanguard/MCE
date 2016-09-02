67521f23bac474f18b7743a875e925503ea0ea56
KevinK <kevin.kalish7@gmail.com>
2016-09-02 18:21:23 +0000

    Transaction History changes and start to filtering transactions by Month

--- public/js/controllers/bankingSimulatorController.js
+++ public/js/controllers/bankingSimulatorController.js
@@ -5,10 +5,11 @@
     'use strict'
     angular.module('bankingSimulatorController', ['studentService','transactionService'])
     .controller('bankingSimulatorController', ['$scope','$http', 'studentService','transactionService', '$q', function($scope, $http, studentService,transactionService, $q) {
+            $scope.selectedMonthTransactions = {};
             $scope.modal = {};
             $scope.selectedStudent;
-            $scope.selectedMonth = new Date().getMonth()-1;
-            $scope.fullMonthName= ["January","Feburary","March","April","May","June","July",
+            $scope.selectedMonth = new Date().getMonth();
+            $scope.fullMonthName= ["","January","Feburary","March","April","May","June","July",
                                     "August","September","October","November","December"];
        
         $scope.loadStudentData = function() {
@@ -22,6 +23,7 @@
             var transactionDataPromise = [transactionService.getTransactions($scope.transactions)];
             $q.all(transactionDataPromise).then(function(transactionDataArray) {
                 $scope.transactionData = transactionDataArray[0].data;
+                $scope.setSelectedMonth(10);
             })
         };
         
@@ -49,24 +51,47 @@
          // if student call setSelectedStudent and send studentData[0] (for when student logs in)
        };
        
-       $scope.setMonth= function(monthIncrement){
-           alert("monthIncrement: " + monthIncrement);
-           if(monthIncrement === 1){
-               // increase month by 1
-               alert("is 1");
-           }else if (monthIncrement === -1){
-               // decrease month by 1
-               alert("is -1");
+       $scope.setSelectedMonth= function(monthIncrement){
+           if(monthIncrement == 1){
+               if($scope.selectedMonth == 12){
+                   $scope.selectedMonth = 1;
+               }else{
+                   $scope.selectedMonth = $scope.selectedMonth + 1;
+               }
+           }else if (monthIncrement == -1){
+               if($scope.selectedMonth == 1){
+                    $scope.selectedMonth = 12;
+               }else{
+                    $scope.selectedMonth = $scope.selectedMonth - 1;
+               }
            }else{
                // no month set, set to current month
-               $scope.selectedMonth = $scope.currentDate.getMonth();
-               alert("selectedMonth: " + $scope.selectedMonth);
-               
+               $scope.selectedMonth = $scope.selectedMonth;
            }
-           
+        //   $scope.sortTransactionsByMonth();
+        //   $scope.apply();
        };
+       
+      $scope.sortTransactionsByMonth = function(){
+          var dateOfTransaction,i, transaction, monthOfTransaction, numOfTransactions = $scope.transactionData.length;
+          $scope.selectedMonthTransactions = {};
+          for (i= 0; i<= numOfTransactions; i++){
+              transaction = $scope.transactionData[i];
+              dateOfTransaction = transaction.date.split("/");
+              return dateOfTransaction[0];
+             // monthOfTransaction = dateOfTransaction[0].substring(1,2);
+            //   if(monthOfTransaction == $scope.selectedMonth){
+            //       $scope.selectedMonthTransactions.push(transaction);
+            //   }
+          }
+      };
+      
+      
+      
+       
         
         $scope.loadStudentData();
         $scope.loadTransactionData();
+        //$scope.setSelectedMonth(10);
     }]);
 }(window.angular));
